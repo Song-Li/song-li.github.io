1ad9c09a272a718661818046e4e12a4968fbb5a7
wushangyangguang <993793072@qq.com>
2016-06-15 17:05:00 -0400

    add new!

--- js/earth.js
+++ js/earth.js
@@ -1,3 +1,21 @@
+function getDistance(lat1,lon1,lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1); 
+    var a = 
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+        Math.sin(dLon/2) * Math.sin(dLon/2)
+        ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return Math.round(d*10000)/10000;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI/180)
+}
+
 var map;
 var city;
 var lati,longi;
@@ -12,16 +30,20 @@ function init() {
         city = data.city;
         region = data.region_name;
         country = data.country_name;
-        console.log(data);
         getMap(lati, longi, city);
     } });
 }
+function getAns(dis){
+    if(dis < 100) return "Wow! You're my neighbor!";
+    if(dis < 1000) return "Wow! You're not far from me!";
+    if(dis < 10000) return "Wow! It's a long distance between us";
+}
 
 function getMap(lati, longi, city){
-    console.log(lati, longi);
+
     map = WE.map('map', {
         center: [mlati, mlongi],
-        zoom: 4,
+        zoom: 2.5,
         dragging: true,
         scrollWheelZoom: true,
         proxyHost: 'http://srtm.webglearth.com/cgi-bin/corsproxy.fcgi?url='
@@ -56,13 +78,26 @@ function getMap(lati, longi, city){
 
     document.getElementById('addmarkers').disabled = true;
     var mmarker = WE.marker([mlati, mlongi]);
-    mmarker.bindPopup("<b>I'm here<b><span style='font-size:10px;color:#999'></br>" + mcity + "</span>", {maxWidth: 150, closeButton: true}).openPopup();
+    mmarker.bindPopup("<b>I'm here<b><span style='font-size:10px;color:#999'></br>" + mcity + "</span>", {maxWidth: 50, closeButton: true}).openPopup();
     mmarker.addTo(map);
-
-    map.panTo([lati, longi]);
 
     var ymarker = WE.marker([lati, longi]);
     ymarker.bindPopup("<b>You're here<b><span style='font-size:10px;color:#999'></br>" + city + ' ' + region + ' ' + country + "</span>", {maxWidth: 150, closeButton: true}).openPopup();
     ymarker.addTo(map);
+
+    var marker = WE.marker([90, 90]);
+    var distance = getDistance(lati, longi, mlati, mlongi);
+    marker.bindPopup("<b>You're " + distance + "km away from me<b><span style='font-size:10px;color:#999'></br>" + getAns(distance) + "</span>", {maxWidth: 200, closeButton: true}).openPopup();
+
+    marker.addTo(map);
+
+    var before = null;
+    requestAnimationFrame(function animate(now) {
+        var c = map.getPosition();
+        var elapsed = before? now - before: 0;
+        before = now;
+        map.setCenter([c[0], c[1] + 0.4*(elapsed/30)]);
+        requestAnimationFrame(animate);
+    });
 
 }
