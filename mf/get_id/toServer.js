d2e3f45dafe6837bb71d28f0d838980f7724f476
songli <993793072@qq.com>
2016-06-22 17:40:15 +0000

    new

---
+++ mf/get_id/toServer.js
@@ -0,0 +1,28 @@
+var ip_address = "184.73.16.65"
+
+function generateUID(){
+    postData = 'GetUID';
+    /*
+    var f = document.createElement("form");
+    f.setAttribute('method',"post");
+    f.setAttribute('action',"http://" + ip_address + "/result.py");
+
+    var i = document.createElement("input"); //input element, text
+    i.setAttribute('type',"text");
+    i.setAttribute('name',"Refresh");
+
+    f.appendChild(i);
+
+    f.submit();
+    return ;
+*/
+    $.ajax({
+        url:"http://" + ip_address + "/result.py",
+        dataType:"text",
+        type: 'POST',
+        data: postData,
+        success:function(data) {
+            console.log(data);
+        }
+    });
+}
