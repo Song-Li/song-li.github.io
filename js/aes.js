ac8baa50c51259e06751944e1c1ae4b3d00d1172
songli <993793072@qq.com>
2016-06-27 20:30:17 +0000

    normal push

--- js/aes.js
+++ js/aes.js
@@ -1,9 +1,19 @@
 function getDecrypt(){ 
     var code = document.getElementById('code').value;
-    var key = document.getElementById('key').value;
+    var key = document.getElementById('key_d').value;
 
     var decrypted = CryptoJS.AES.decrypt(code, key);
     ////4d657373616765
     //
     document.getElementById("Message").innerHTML = decrypted.toString(CryptoJS.enc.Utf8);
+}
+
+function getEncrypt(){ 
+    var code = document.getElementById('encryption').value;
+    var key = document.getElementById('key_e').value;
+
+    var encrypted = CryptoJS.AES.encrypt(code, key);
+    ////4d657373616765
+    //
+    document.getElementById("Result").innerHTML = encrypt;
 }
