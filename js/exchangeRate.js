586827066d6a08b4d89255da613b0949edf8c89b
songli <993793072@qq.com>
2016-06-28 16:02:35 +0000

    normal push

--- js/exchangeRate.js
+++ js/exchangeRate.js
@@ -3,4 +3,8 @@ $.ajax({ url: 'http://api.fixer.io/latest?base=USD', success: function(data) {
     for (var key in data) {
         $('#origin').append("<div>" + key + "->" + data[key]);
     }
+    for (var key in data['rates']) {
+        a('#origin').append("<div>" + key + "->" + data['rates'][key]);
+    }
+
 } });
