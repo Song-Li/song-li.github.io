267345685510f1cc0b67f786b96f6e8c96642031
Song Li <993793072@qq.com>
2016-11-02 17:59:22 -0400

    Update scriptDownload.js

--- tests/scriptDownload.js
+++ tests/scriptDownload.js
@@ -17,6 +17,7 @@ function run(cur_size) {
   window.onerror = function(e) {
     var end = performance.now();
     var res = end - start;
+    console.log("size: " + cur_size.toString() + " round: " + cur.toString() + " start: " + start.toString() + " end: " + end.toString(), end - start);
     addToRes(cur_size, res); 
     if(cur < payload) run(cur_size);
     else if(cur_size < max_size) doJob(++ cur_size); 
