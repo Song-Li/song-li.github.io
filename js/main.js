802b66465f24af22f60b58525007c7422dfeb4e0
Song Li <993793072@qq.com>
2016-12-01 11:45:14 -0500

    normal push

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-$(document).ready(function(){
-	$('#main-img').addClass('animated zoomIn') ;
-}) ;
+$("#left-mask").hover (
+  function(event) {
+    $('.left').css({"color":"#000"});
+    $('.right').css({"color":"#CFCFCF"});
+  },
+  function(event) {
+    $('.left').css({"color":"#CFCFCF"});
+  } 
+)
+
+$("#right-mask").hover (
+  function(event) {
+    $('.left').css({"color":"#CFCFCF"});
+    $('.right').css({"color":"#000"});
+  },
+  function(event) {
+    $('.right').css({"color":"#CFCFCF"});
+  } 
+)
